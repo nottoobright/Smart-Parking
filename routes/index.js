4f8b0c7a0d07f1e9654f0ce43c981d84672f5278
nottoobright <puthranyash1@gmail.com>
2017-12-22 09:17:31 +0530

    Added firebase ParkStatus check

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../controllers/userController');
-const auth = require('../controllers/authController');
+const passport = require('passport');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 /* GET home page. */
-router.get('/', auth.isLoggedIn, (req, res, next) => { res.render("index"), { title: "SmartParking" } });
+router.get('/', isLoggedIn , (req, res, next) => { res.render("index"), { title: "SmartParking" } });
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 module.exports = router;
